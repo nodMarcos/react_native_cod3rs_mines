baa76c375fb6245b90d9477be88c54c300f3629e
Marcos <nodmarcos@gmail.com>
2022-05-12 00:48:53 -0300

    Creation of the fields layouts and some functions

--- App.js
+++ App.js
@@ -3,30 +3,50 @@ import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field';
 import Flag from './src/components/Flag';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/functions';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>iniciando o Mines</Text>
-      <Text>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-      {/* <Field />
-      <Field opened/>
-      <Field opened nearMines={1}/>
-      <Field opened nearMines={2}/>
-      <Field opened nearMines={3}/>
-      <Field opened nearMines={6}/>
-      <Field mined />
-      <Field mined opened/>
-      <Field mined opened exploded/>
-      <Field flagged />
-      <Field flagged opened/> */}
+export default class App extends React.Component {
 
-      <Flag />
-      <Flag bigger/>
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
 
-    </View>
-  );
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text style={styles.welcome}>iniciando o Mines</Text>
+        <Text style={styles.instruction}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>        
+  
+      </View>
+    );
+
+  }
+  
 }
 
 const styles = StyleSheet.create({
@@ -36,4 +56,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  board: {
+    alignItems: 'center',
+    backgroundColor:'#aaa'
+  }
 });
